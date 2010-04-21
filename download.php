b9c5a9c81230d1099d7c495d315423e9a95c39b4
John Beranek <jberanek@users.sourceforge.net>
2010-04-21 22:11:38 +0000

    Update a few SF links

--- download.php
+++ download.php
@@ -8,17 +8,17 @@ page_header(NULL, "Download");
 
 <ul>
   <li>
-    <a href="http://sourceforge.net/project/showfiles.php?group_id=5113">
-      Download Tarball</a>
+    <a href="http://sourceforge.net/projects/mrbs/files">
+      Download Tarball/Zip</a>
   </li>
   <li>
-    <a href="http://sourceforge.net/svn/?group_id=5113">
+    <a href="http://sourceforge.net/scm/?type=svn&amp;group_id=5113">
       SVN Instructions</a>
   </li>
 </ul>
 <p>
   The latest releases will always be available
-  <A href="http://sourceforge.net/project/showfiles.php?group_id=5113">
+  <A href="http://sourceforge.net/projects/mrbs/files">
     on the Sourceforge download page</a> along with release notes
   describing what's changed. Just find the latest release and click the
   &quot;Download&quot; link by the &quot;mrbs&quot; package like this :
@@ -27,7 +27,7 @@ page_header(NULL, "Download");
    alt="Picture of SF download page" width="505" height="107"> 
 <p>
   Alternatively you can get MRBS via SVN,
-  instructions are <a href="http://sourceforge.net/svn/?group_id=5113">here</a>.
+  instructions are <a href="http://sourceforge.net/scm/?type=svn&amp;group_id=5113">here</a>.
   Releases are currently made from the trunk <b>mrbs/trunk</b>,
   but further minor releases are likely to be made on a stable
   branch.
