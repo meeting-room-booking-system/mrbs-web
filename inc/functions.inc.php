5a65e85eac33da07be094fd21d3c199db03274f4
John Beranek <jberanek@users.sourceforge.net>
2017-10-01 10:55:48 +0100

    Update to HTTPS

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -79,7 +79,7 @@ function page_footer()
     <object><noscript><p><img src="https://apps.sourceforge.net/piwik/mrbs/piwik.php?idsite=1" alt="piwik"/></p></noscript></object>
     <!-- End Piwik Tag --> <!-- Piwik --> 
     <script type="text/javascript">
-    var pkBaseURL = (("https:" == document.location.protocol) ? "https://mrbs.sourceforge.net/piwik/" : "http://mrbs.sourceforge.net/piwik/");
+    var pkBaseURL = (("https:" == document.location.protocol) ? "https://mrbs.sourceforge.io/piwik/" : "http://mrbs.sourceforge.io/piwik/");
     document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
     </script><script type="text/javascript">
     try {
@@ -87,7 +87,7 @@ function page_footer()
     piwikTracker.trackPageView();
     piwikTracker.enableLinkTracking();
     } catch( err ) {}
-    </script><noscript><p><img src="https://mrbs.sourceforge.net/piwik/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+    </script><noscript><p><img src="https://mrbs.sourceforge.io/piwik/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
     <!-- End Piwik Tracking Code -->
     
   </body>
