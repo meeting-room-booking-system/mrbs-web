d94fa160a0626e6f9ed565d3c02ff91ebc4c00c6
John Beranek <jberanek@users.sourceforge.net>
2021-11-29 21:18:33 +0000

    Moved demo sites.

--- demo.php
+++ demo.php
@@ -8,10 +8,10 @@ page_header(NULL, "Demo");
 ?>
 
 <ul>
-  <li><a href="mrbs/">Demonstration installation</a></li>
-  <li><a href="periodsmrbs/">Demonstration using periods</a></li>
-  <li><a href="mrbs/admin.php">Area/room configuration</a></li>
-  <li><a href="mrbs/help.php">Additional Help</a></li>
+  <li><a href="https://mrbsdemo.uk.to/mrbs/">Demonstration installation</a></li>
+  <li><a href="https://mrbsdemo.uk.to/periodmrbs/">Demonstration using periods</a></li>
+  <li><a href="https://mrbsdemo.uk.to/mrbs/admin.php">Area/room configuration</a></li>
+  <li><a href="https://mrbsdemo.uk.to/mrbs/help.php">Additional Help</a></li>
 </ul>
 <p>
   These installations should always be running the latest released version.
@@ -23,11 +23,11 @@ page_header(NULL, "Demo");
 <p>
   The installations are a standard installation of MRBS, using the simpler
   'config' authentication system, to stop people deleting users.
-  You can use the <a href="mrbs/admin.php">area/room configuration</a>
+  You can use the <a href="https://mrbsdemo.uk.to/mrbs/admin.php">area/room configuration</a>
   system, although we'd prefer it if you didn't delete other users' configuration!
   You can login as any of the following users, only the user "admin" has
   administrative rights. Additional help is available under the
-  <a href="mrbs/help.php">help</a> link at the top of the page.
+  <a href="https://mrbsdemo.uk.to/mrbs/help.php">help</a> link at the top of the page.
 </p>
 
 <table class="prettytable">
