39b90774742ea3d0b84a871f651bc38c2bd9bfaf
John Beranek <jberanek@users.sourceforge.net>
2023-09-11 21:15:53 +0100

    Updated contact page to only mention GitHub issue tracker

--- contact.php
+++ contact.php
@@ -8,45 +8,14 @@ page_header(NULL, "Contact");
 
 <p>
   The preferred method of getting support from the MRBS development team is
-  via the <a href="https://sourceforge.net/p/mrbs/support-requests/">Support Requests</a>
-  tracker at Sourceforge.
+  via the <a href="https://github.com/meeting-room-booking-system/mrbs-code/issue">GitHub issue tracker</a>.
 </p>
-<p>
-  You can also ask questions on the the main MRBS
-  mailing list, <a href="mailto:mrbs-general@lists.sourceforge.net">
-    mrbs-general@lists.sourceforge.net</a>. You can send absolutely anything
-  there - you can sign up or browse the archives from the
-  <a href="https://sourceforge.net/projects/mrbs/lists/mrbs-general">
-    mailing list management page</a>. <b>Note: You will not be able to
-  post to the list until you subscribe, but you can subscribe and
-  disable email delivery (or choose to receive digests) if you prefer.
-  This is a measure to keep the mailing list free of spam.</b>
-</p>
-
-<p>
-  We run a number of different trackers at Sourceforge to track different sorts
-  of things, please try to choose the appropriate one from the below:
-</p>
-<ul>
-  <li>
-    <a href="https://sourceforge.net/p/mrbs/support-requests/">Support Requests</a>
-  </li>
-  <li>
-    <a href="https://sourceforge.net/p/mrbs/bugs/">Bugs</a>
-  </li>
-  <li>
-    <a href="https://sourceforge.net/p/mrbs/patches/">Patches</a>
-  </li>
-  <li>
-    <a href="https://sourceforge.net/p/mrbs/feature-requests/">Feature Requests</a>
-  </li>
-</ul>
 
 <p>
   Please do <b>not</b> contact the MRBS developers or administrators directly -
   we generally only work on MRBS in our spare time, and prefer not to receive
   personal email from users directly. Emailing one person directly rather
-  than using the mailing list or trackers will also reduce your chance of getting
+  than using the issue tracker will also reduce your chance of getting
   a useful reply in a timely fashion.
 </p>
               
